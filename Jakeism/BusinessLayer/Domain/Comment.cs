aa78bc212f93c1aa42c41dfd68e57aa6d92c9bbf
jacob.nlsn <jacob.nlsn@gmail.com>
2011-04-05 16:57:58 +0000

    Mapping and Domain Object finished for Comment.

--- Jakeism/BusinessLayer/Domain/Comment.cs
+++ Jakeism/BusinessLayer/Domain/Comment.cs
@@ -7,5 +7,41 @@ namespace BusinessLayer.Domain
 {
     public class Comment : DomainBase
     {
+        private Entry entry;
+
+        private string commentBody;
+
+        private User usr;
+
+        private DateTime date;
+
+        public virtual Entry Entry
+        {
+            get { return this.entry; }
+
+            set { this.entry = value; }
+        }
+
+        public virtual string CommentBody
+        {
+            get { return this.commentBody; }
+
+            set { this.commentBody = value; }
+        }
+
+        public virtual User Usr
+        {
+            get { return this.usr; }
+
+            set { this.usr = value; }
+        }
+
+        public virtual DateTime Date
+        {
+            get { return this.date; }
+
+            set { this.date = value; }
+        }
+
     }
 }
