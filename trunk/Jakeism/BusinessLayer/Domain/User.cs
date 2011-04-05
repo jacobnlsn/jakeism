e7791068f3471064f20c5d1b244ed05754e720e7
jacob.nlsn@gmail.com <jacob.nlsn@gmail.com@4b167144-2661-05c7-878f-4124802e2292>
2011-04-05 17:34:50 +0000

    Mapping file and object file completed for User.

--- trunk/Jakeism/BusinessLayer/Domain/User.cs
+++ trunk/Jakeism/BusinessLayer/Domain/User.cs
@@ -1,11 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Iesi.Collections.Generic;
 
 namespace BusinessLayer.Domain
 {
     public class User : DomainBase
     {
+        private string userName;
+
+        private string password;
+
+        private Boolean isAdmin;
+
+        private ISet<Entry> votes;
+
+        public User()
+        {
+            this.votes = new HashedSet<Entry>();
+        }
+
+        public virtual string UserName
+        {
+            get { return this.userName; }
+
+            set { this.userName = value; }
+        }
+
+        public virtual string Password
+        {
+            get { return this.password; }
+
+            set { this.password = value; }
+        }
+
+        public virtual Boolean IsAdmin
+        {
+            get { return this.isAdmin; }
+
+            set { this.isAdmin = value; }
+        }
+
+        public virtual ISet<Entry> Votes
+        {
+            get { return this.votes; }
+
+            set { this.votes = value; }
+        }
     }
 }
