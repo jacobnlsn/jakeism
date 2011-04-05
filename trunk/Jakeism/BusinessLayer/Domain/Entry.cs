0ddd8c83a22ab0a726a896db8042a0fdaffc33dc
jacob.nlsn@gmail.com <jacob.nlsn@gmail.com@4b167144-2661-05c7-878f-4124802e2292>
2011-04-05 17:17:25 +0000

    Mapping and Domain Object finished for Entry.

--- trunk/Jakeism/BusinessLayer/Domain/Entry.cs
+++ trunk/Jakeism/BusinessLayer/Domain/Entry.cs
@@ -1,11 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Iesi.Collections.Generic;
 
 namespace BusinessLayer.Domain
 {
     public class Entry : DomainBase
     {
+        private string entryBody;
+
+        private User usr;
+
+        private DateTime date;
+
+        private ISet<User> votes;
+
+        public Entry()
+        {
+            this.votes = new HashedSet<User>();
+        }
+
+        public virtual string EntryBody
+        {
+            get { return this.entryBody; }
+
+            set { this.entryBody = value; }
+        }
+
+        public virtual User Usr
+        {
+            get { return this.usr; }
+
+            set { this.usr = value; }
+        }
+
+        public virtual DateTime Date
+        {
+            get { return this.date; }
+
+            set { this.date = value; }
+        }
+
+        public virtual ISet<User> Votess
+        {
+            get { return this.votes; }
+
+            set { this.votes = value; }
+        }
+
+        public virtual void AddVote(User vote)
+        {
+            this.votes.Add(vote);
+        }
     }
 }
