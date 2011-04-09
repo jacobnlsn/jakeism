509edcae4b76c78346f7d3618e165f7b095348e9
ttreat31 <ttreat31@gmail.com>
2011-04-09 04:29:16 +0000

    Restored homepage

--- Jakeism/Jakeism/Default.aspx.cs
+++ Jakeism/Jakeism/Default.aspx.cs
@@ -12,21 +12,10 @@ namespace Jakeism
     public partial class _Default : System.Web.UI.Page
     {
 
-        HibernateService service = new HibernateService();
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            ListOfEntries.DataSource = service.GetAllRecords(typeof(Entry));
-            ListOfEntries.DataBind();
+
         }
 
-        protected void SubmitEntry_Click(object sender, EventArgs e)
-        {
-            Entry newEntry = new Entry();
-            newEntry.Date = DateTime.Now;
-            newEntry.EntryBody = JakeismEntry.Text;
-            newEntry.User = null;
-            service.Save(newEntry);
-        }
     }
 }
