ac18c6ca36ef527d960bff80598ed1399cca0e64
jacob.nlsn <jacob.nlsn@gmail.com>
2011-04-18 02:22:45 +0000

    Added verification of the text.

--- Jakeism/Jakeism/Admin/ManageEntry.aspx.cs
+++ Jakeism/Jakeism/Admin/ManageEntry.aspx.cs
@@ -96,10 +96,22 @@ namespace Jakeism.Admin
 
         protected void Modify_Entry(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(entryBody.Text.Trim()))
+            {
+                fail.Text = "Entry field may not be empty";
+                fail.Visible = true;
+                return;
+            }
+            if (entryBody.Text.Trim().Length > 255)
+            {
+                fail.Text = "Entry may not exceed 255 characters";
+                fail.Visible = true;
+                return;
+            }
             using (var service = new HibernateService())
             {
                 Entry entry = (Entry)service.FindById(Id, typeof(Entry));
-                entry.EntryBody = entryBody.Text;
+                entry.EntryBody = entryBody.Text.Trim();
                 service.SaveOrUpdate(entry);
             }
         }
