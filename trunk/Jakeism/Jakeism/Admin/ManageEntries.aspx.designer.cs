feb0919be63249b50449dc45dc508c714c9cce01
jacob.nlsn@gmail.com <jacob.nlsn@gmail.com@4b167144-2661-05c7-878f-4124802e2292>
2011-04-18 23:56:46 +0000

    I Broke every thing. Fix it tyler. ManageEntries.aspx.cs [Delete_Entries].

--- trunk/Jakeism/Jakeism/Admin/ManageEntries.aspx.designer.cs
+++ trunk/Jakeism/Jakeism/Admin/ManageEntries.aspx.designer.cs
@@ -29,14 +29,5 @@ namespace Jakeism.Admin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button delete;
-        
-        /// <summary>
-        /// update control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button update;
     }
 }
