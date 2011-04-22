c279d14f9d6fb40218b0e3a20ac09f4eb81979f2
ttreat31 <ttreat31@gmail.com>
2011-04-22 06:47:14 +0000

    More styling

--- Jakeism/Jakeism/ViewEntry.aspx.designer.cs
+++ Jakeism/Jakeism/ViewEntry.aspx.designer.cs
@@ -85,15 +85,6 @@ namespace Jakeism.Entries {
         protected global::System.Web.UI.WebControls.TextBox commentBox;
         
         /// <summary>
-        /// fail control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label fail;
-        
-        /// <summary>
         /// submit control.
         /// </summary>
         /// <remarks>
@@ -101,5 +92,14 @@ namespace Jakeism.Entries {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button submit;
+        
+        /// <summary>
+        /// fail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label fail;
     }
 }
