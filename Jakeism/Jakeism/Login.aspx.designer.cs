c911b224b4933403cad7d9ed0c45c0b6be0c0d90
ttreat31 <ttreat31@gmail.com>
2011-04-26 05:15:06 +0000

    Enhanced Registration and Login

--- Jakeism/Jakeism/Login.aspx.designer.cs
+++ Jakeism/Jakeism/Login.aspx.designer.cs
@@ -49,12 +49,12 @@ namespace Jakeism.Users {
         protected global::System.Web.UI.WebControls.CheckBox remember;
         
         /// <summary>
-        /// notFound control.
+        /// fail control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label notFound;
+        protected global::System.Web.UI.WebControls.Label fail;
     }
 }
