f26d7e90361362620fe62411497b862603a23153
ttreat31@gmail.com <ttreat31@gmail.com@4b167144-2661-05c7-878f-4124802e2292>
2011-04-26 07:01:44 +0000

    Enhanced ViewUser.aspx

---
+++ trunk/Jakeism/Jakeism/Scripts/tabs.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    var tabContainers = $('div.tabs > div');
+
+    $('div.tabs ul.tabNavigation a').click(function () {
+        tabContainers.hide().filter(this.hash).show();
+
+        $('div.tabs ul.tabNavigation a').removeClass('selected');
+        $(this).addClass('selected');
+
+        return false;
+    }).filter(':first').click();
+});
