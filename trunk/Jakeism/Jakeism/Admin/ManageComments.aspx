86737af5df765e9ffe2617c06a307a08fe013d54
jacob.nlsn@gmail.com <jacob.nlsn@gmail.com@4b167144-2661-05c7-878f-4124802e2292>
2011-04-29 04:42:32 +0000

    Work on manage comments.

--- trunk/Jakeism/Jakeism/Admin/ManageComments.aspx
+++ trunk/Jakeism/Jakeism/Admin/ManageComments.aspx
@@ -2,4 +2,32 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+<p><a href="Default.aspx">admin panel</a> > manage comments</p>
+    <h2>Manage Entries</h2>
+
+    <br /><br />
+
+    <asp:ListView runat="server" ID="comments">
+
+        <LayoutTemplate>
+            <asp:PlaceHolder runat="server" ID="itemPlaceholder" />
+        </LayoutTemplate>
+
+        <ItemTemplate>
+            <asp:CheckBox runat="server" ID="check" />
+            ID: <%# Eval("Id") %><br />
+            <%# Eval("CommentBody") %><br />
+            Author: <%# Eval("User.UserName") %><br />
+            Date: <%# Eval("Date") %><br />
+            <asp:Button ID="ModifyCommentButton" runat="server" Text="Modify Comment" OnClick="Modify_Comment" CommandArgument='<%# Eval("Id") %>' />
+            <br />
+        </ItemTemplate>
+
+        <EmptyDataTemplate>
+        <div>No comments</div>
+    </EmptyDataTemplate>
+
+    </asp:ListView>
+
+    <asp:Button runat="server" ID="delete" Text="Delete Selected Comments" OnClick="Delete_Comments" />
 </asp:Content>
--- trunk/Jakeism/Jakeism/Admin/ManageComments.aspx.cs
+++ trunk/Jakeism/Jakeism/Admin/ManageComments.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusinessLayer.Service;
+using BusinessLayer.Domain;
 
 namespace Jakeism.Admin
 {
@@ -11,7 +13,78 @@ namespace Jakeism.Admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ((Admin)this.Master).checkIfAdmin(Page);
+            if (!IsPostBack)
+            {
+                PopulateComments(Request.QueryString["type"], Request.QueryString["id"]);
+            }
+        }
 
+        private void PopulateComments(string type, string tempId)
+        {
+            List<Comment> allComments = null;
+            List<Comment> returnComments = new List<Comment>();
+            using (var service = new HibernateService())
+            {
+            
+            long id = 0;
+            if (tempId != null)
+            {
+                id = Int64.Parse(tempId);
+            }
+            
+                allComments = (List<Comment>)service.GetAllRecords<Comment>();
+            
+            if (type == "entry")
+            {
+                foreach (Comment currentComment in allComments)
+                {
+                    if (currentComment.Entry.Id == id)
+                    {
+                        returnComments.Add(currentComment);
+                    }
+                }
+            }
+            if (type == "user")
+            {
+                foreach (Comment currentComment in allComments)
+                {
+                    if (currentComment.User.Id == id)
+                    {
+                        returnComments.Add(currentComment);
+                    }
+                }
+            }
+            }
+
+            comments.DataSource = returnComments;
+            comments.DataBind();
+        }
+
+        protected void Delete_Comments(object sender, EventArgs e)
+        {
+            List<ListViewDataItem> theComments = (List<ListViewDataItem>)comments.Items;
+            foreach (ListViewDataItem currentEntry in theComments)
+            {
+                CheckBox box = (CheckBox)currentEntry.FindControl("check");
+                Button button = (Button)currentEntry.FindControl("ModifyButton");
+                string id = button.CommandArgument;
+                if (box.Checked)
+                {
+                    using (var service = new HibernateService())
+                    {
+                        Comment toDelete = (Comment)service.FindById(Int64.Parse(id), typeof(Comment));
+                        service.Delete(toDelete);
+                    }
+                }
+            }
+            PopulateComments(Request.QueryString["type"], Request.QueryString["id"]);
+        }
+
+        protected void Modify_Comment(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            Response.Redirect("ManageComment.aspx?id=" + button.CommandArgument);
         }
     }
 }
