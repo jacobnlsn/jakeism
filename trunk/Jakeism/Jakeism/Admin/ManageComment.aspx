96fb39eaa45a452bf6022392d25b1acdae606ef7
jacob.nlsn@gmail.com <jacob.nlsn@gmail.com@4b167144-2661-05c7-878f-4124802e2292>
2011-04-29 05:27:56 +0000

    Manage Comments complete.

---
+++ trunk/Jakeism/Jakeism/Admin/ManageComment.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="ManageComment.aspx.cs" Inherits="Jakeism.Admin.ManageComment" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+
+    <script type="text/javascript" src="../scripts/jquery-1.4.1.js"></script>
+    <script type="text/javascript" src="../scripts/forms.js"></script>
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <asp:Label runat="server" ID="title" /><br />
+    <h2>Modify Comment</h2>
+    <asp:TextBox runat="server" ID="commentBody" Width="500" Height="100" TextMode="MultiLine" CssClass="word_count" />
+    <br /><span class="counter"></span><br />
+    <asp:Label runat="server" ID="fail" Visible="false" /><br />
+    <asp:Button ID="submit" runat="server" Text="Modify" OnClick="Modify_Comment" />
+
+</asp:Content>
---
+++ trunk/Jakeism/Jakeism/Admin/ManageComment.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLayer.Service;
+using BusinessLayer.Domain;
+
+namespace Jakeism.Admin
+{
+    public partial class ManageComment : System.Web.UI.Page
+    {
+
+        protected long Id
+        {
+            get;
+            set;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ((Admin)this.Master).checkIfAdmin(Page);
+
+            using (var service = new HibernateService())
+            {
+                Comment comment = null;
+                if (Request.QueryString["id"] != null)
+                {
+                    long id = Int64.Parse(Request.QueryString["id"]);
+                    comment = service.FindById<Comment>(id);
+                }
+                if (comment == null)
+                {
+                    title.Text = "Jakeism Not Found";
+                    commentBody.Visible = false;
+                    submit.Visible = false;
+                }
+                else
+                {
+                    Id = comment.Id;
+                    title.Text = "<h2>Jakeism #" + comment.Id + "</h2>";
+                    if (!IsPostBack)
+                    {
+                        commentBody.Text = comment.CommentBody;
+                    }
+                }
+            }
+        }
+
+        protected void Modify_Comment(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(commentBody.Text.Trim()))
+            {
+                fail.Text = "Entry field may not be empty";
+                fail.Visible = true;
+                return;
+            }
+            if (commentBody.Text.Trim().Length > 255)
+            {
+                fail.Text = "Entry may not exceed 255 characters";
+                fail.Visible = true;
+                return;
+            }
+            using (var service = new HibernateService())
+            {
+                Comment comment = (Comment)service.FindById(Id, typeof(Comment));
+                comment.CommentBody = commentBody.Text.Trim();
+                service.SaveOrUpdate(comment);
+                commentBody.Text = comment.CommentBody;
+            }
+
+
+        }
+    }
+}
